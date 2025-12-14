fc0536f0efbd15893b1534441551a808ec009f98
Robson Tenório <rrtenorio@gmail.com>
2025-12-14 11:04:56 -0300

    Add statements to drop extensions if they exist

--- database/migrations/0001_01_01_000004_create_extensions.php
+++ database/migrations/0001_01_01_000004_create_extensions.php
@@ -13,6 +13,8 @@ return new class extends Migration {
         $schema_dot = $schema ? "$schema." : '';
 
         // Extensões
+        DB::statement('DROP EXTENSION IF EXISTS unaccent;');
+        DB::statement('DROP EXTENSION IF EXISTS pg_trgm;');
         DB::statement("CREATE EXTENSION IF NOT EXISTS unaccent {$schema_name}");
         DB::statement("CREATE EXTENSION IF NOT EXISTS pg_trgm {$schema_name}");
 
